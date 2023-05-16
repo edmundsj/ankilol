487f1158616486de75af2a0ea78f10834e46c7ca
Jordan <edmundsj@uci.edu>
2023-05-16 10:51:18 -0400

    added connector.py from AnkiConnect documentation at https://foosoft.net/projects/anki-connect/index.html#note-actions

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
 from .parser import extract_entries, write_entries
+from .connector import add_note
 
 
 def main(filename: str):
     # Use a breakpoint in the code line below to debug your script.
     answered, unanswered = extract_entries(filename)
     write_entries(filename='remaining_questions.txt', entries=unanswered)
+    if len(answered) > 0:
+        add_note(answered[0])
 
 
 # Press the green button in the gutter to run the script.
