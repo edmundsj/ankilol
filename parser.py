06b050f62f2454a329c5b1cc20a9a8e22e937163
Jordan <edmundsj@uci.edu>
2023-06-11 09:01:15 -0400

    Added HTML document parsing ability, refactored existing parsing logic, moved it outside individual parsers

--- parser.py
+++ parser.py
@@ -1,61 +1,116 @@
+import typing
 from collections import namedtuple
-from typing import TextIO
+from typing import TextIO, Callable, Any, TypeVar, Generic
+from abc import ABC
+
+import bs4
+from bs4 import BeautifulSoup
 
 Entry = namedtuple('Entry', ['question', 'answer'])
 
 
-def write_entries(filename: str, entries: list[Entry]) -> None:
-    with open(filename, 'w') as file:
-        for entry in entries:
-            write_entry(file, entry)
+class GenericParser(ABC):
+    def write_entries(self, entries: list[Entry]):
+        pass
+
+    def extract_entries(self)-> (list[Entry], list[Entry]):
+        pass
 
 
-def write_entry(file: TextIO, entry: Entry):
-    file.write(entry.question + '\n')
-    if entry.answer is not None:
-        file.write('\t' + entry.answer + '\n')
-
-
-def extract_entries(filename: str) -> (list[Entry], list[Entry]):
+def extract_entries(
+        iterable,
+        is_answer: Callable[[Any], bool],
+        parse_question: Callable[[Any], Any],
+        parse_answer: Callable[[Any], Any],
+) -> (list[Entry], list[Entry]):
     answered_questions = []
     unanswered_questions = []
     current_question = None
 
-    num_lines = get_num_lines(filename)
-    with open(filename, 'r') as file:
-        for index, line in enumerate(file):
-            is_last_line = index == num_lines - 1
-            if line == '\n' and not is_last_line:
-                continue
-            if is_answer(line):
-                new_entry = Entry(question=current_question, answer=parse_answer(line))
-                answered_questions.append(new_entry)
-                current_question = None
-            else:
-                if current_question is not None:
-                    new_entry = Entry(question=current_question, answer=None)
-                    unanswered_questions.append(new_entry)
-                current_question = parse_question(line)
+    for index, line in enumerate(iterable):
+        is_last_line = index == len(iterable) - 1
+        if is_answer(line):
+            new_entry = Entry(question=current_question, answer=parse_answer(line))
+            answered_questions.append(new_entry)
+            current_question = None
+        elif not is_answer(line) and not is_last_line:
+            if current_question is not None:
+                new_entry = Entry(question=current_question, answer=None)
+                unanswered_questions.append(new_entry)
+            current_question = parse_question(line)
+        elif not is_answer(line) and is_last_line:
+            new_entry = Entry(question=parse_question(line), answer=None)
+            unanswered_questions.append(new_entry)
 
     return answered_questions, unanswered_questions
 
 
-def is_answer(line: str) -> bool:
-    valid_starts = ['\t', '  ', '* ', '-']
-    if any([line.startswith(start) for start in valid_starts]):
-        return True
-    else:
+class HTMLParser(GenericParser):
+    def __init__(self, filename: str):
+        self.filename = filename
+
+    def write_entries(self, entries: list[Entry]):
+        pass
+
+    def extract_entries(self) -> (list[Entry], list[Entry]):
+        with open(self.filename, 'r') as fh:
+            soup = BeautifulSoup(fh, 'html.parser')
+            body = soup.body
+            entries = []
+            if body is not None:
+                entries = [element for element in body.find_all(recursive=False)]
+            return extract_entries(entries, self._is_answer, self._parse_question, self._parse_answer)
+        pass
+
+    def _is_answer(self, line: bs4.Tag):
+        if line.name == 'ul':
+            return True
         return False
 
+    def _parse_question(self, element: bs4.Tag):
+        return element.text
 
-def get_num_lines(filename: str) -> int:
-    with open(filename, 'r') as file:
-        return sum(1 for line in file)
-
-
-def parse_question(question: str) -> str:
-    return question.removesuffix('\n')
+    def _parse_answer(self, element: bs4.Tag):
+        return element.text
 
 
-def parse_answer(answer: str) -> str:
-    return answer.removesuffix('\n').removeprefix('\t').strip(' ')
+class TextParser(GenericParser):
+    ANSWER_PREFIXES = ['\t', '  ', '* ']
+
+    def __init__(self, filename: str):
+        self.filename = filename
+
+    def write_entries(self, entries: list[Entry]) -> None:
+        with open(self.filename, 'w') as file:
+            for entry in entries:
+                self._write_entry(file, entry)
+
+    def extract_entries(self) -> (list[Entry], list[Entry]):
+        with open(self.filename, 'r') as file:
+            lines = [line for line in file if line != '\n']
+            return extract_entries(lines, self._is_answer, self._parse_question, self._parse_answer)
+
+    def _write_entry(self, file: TextIO, entry: Entry):
+        file.write(entry.question + '\n')
+        if entry.answer is not None:
+            file.write('\t' + entry.answer + '\n')
+
+    def _is_answer(self, line: str) -> bool:
+        valid_starts = ['\t', '  ', '* ', '-']
+        if any([line.startswith(start) for start in valid_starts]):
+            return True
+        else:
+            return False
+
+    def _get_num_lines(self) -> int:
+        with open(self.filename, 'r') as file:
+            return sum(1 for line in file)
+
+    def _parse_question(self, question: str) -> str:
+        return question.removesuffix('\n')
+
+    def _parse_answer(self, answer: str) -> str:
+        stripped_answer = answer
+        for prefix in self.ANSWER_PREFIXES:
+            stripped_answer = stripped_answer.removeprefix(prefix)
+        return stripped_answer.removesuffix('\n')
