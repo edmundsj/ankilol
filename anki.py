0bdecc11ec14f48ea1191f73c4e9a0984469553d
Jordan <edmundsj@uci.edu>
2023-06-11 12:34:28 -0400

    Added file uploader classes to enable end-to-end download/upload of files from google drive, and anki automatic sync. Full end-to-end can now be run.

--- anki.py
+++ anki.py
@@ -3,55 +3,61 @@ import logging
 import urllib.request
 from .definitions import Entry
 
-BASE_URL = 'http://localhost:8765'
-
-def request(action, **params):
-    return {'action': action, 'params': params, 'version': 6}
 
 
-def invoke(action, **params):
-    request_json = json.dumps(request(action, **params)).encode('utf-8')
-    response = json.load(urllib.request.urlopen(urllib.request.Request(BASE_URL, request_json)))
-    if len(response) != 2:
-        raise Exception('response has an unexpected number of fields')
-    if 'error' not in response:
-        raise Exception('response is missing required error field')
-    if 'result' not in response:
-        raise Exception('response is missing required result field')
-    if response['error'] is not None:
-        logging.error(response['error'])
-    return response['result']
+class AnkiConnect:
 
+    def __init__(self, version=6, base_url='http://localhost:8765'):
+        self.version = version
+        self.base_url = base_url
 
-def is_anki_running():
-    try:
-        response = urllib.request.urlopen(urllib.request.Request(BASE_URL))
-    except Exception as e:
-        return False
-    return True
+    def _request(self, action, **params):
+        return {'action': action, 'params': params, 'version': self.version}
 
+    def _invoke(self, action, **params):
+        request_json = json.dumps(self._request(action, **params)).encode('utf-8')
+        response = json.load(urllib.request.urlopen(urllib.request.Request(self.base_url, request_json)))
+        if len(response) != 2:
+            raise Exception('response has an unexpected number of fields')
+        if 'error' not in response:
+            raise Exception('response is missing required error field')
+        if 'result' not in response:
+            raise Exception('response is missing required result field')
+        if response['error'] is not None:
+            logging.error(response['error'])
+        return response['result']
 
-def add_note(entry: Entry, deck='Web Development'):
-    params = {
-        'note': {
-            "deckName": deck,
-            "modelName": "Basic",
-            "fields": {
-                "Front": entry.question,
-                "Back": entry.answer
-            },
-            "options": {
-                "allowDuplicate": False,
-                "duplicateScope": "deck",
-                "duplicateScopeOptions": {
-                    "deckName": deck,
-                    "checkChildren": False,
-                    "checkAllModels": False
-                }
-            },
-            "tags": [
-            ],
+    def is_running(self):
+        try:
+            response = urllib.request.urlopen(urllib.request.Request(self.base_url))
+        except Exception as e:
+            return False
+        return True
+
+    def sync(self):
+        return self._invoke('sync')
+
+    def add_note(self, entry: Entry, deck='Web Development'):
+        params = {
+            'note': {
+                "deckName": deck,
+                "modelName": "Basic",
+                "fields": {
+                    "Front": entry.question,
+                    "Back": entry.answer
+                },
+                "options": {
+                    "allowDuplicate": False,
+                    "duplicateScope": "deck",
+                    "duplicateScopeOptions": {
+                        "deckName": deck,
+                        "checkChildren": False,
+                        "checkAllModels": False
+                    }
+                },
+                "tags": [
+                ],
+            }
         }
-    }
-    logging.info(f'Adding note {entry.question}')
-    return invoke('addNote', **params)
+        logging.info(f'Adding note {entry.question}')
+        return self._invoke('addNote', **params)
